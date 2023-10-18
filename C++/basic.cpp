143558ca2e9b97702114805fb8dc44736b2b005b
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2023-10-18 18:47:35 +0530

    Add files via upload

---
+++ C++/basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//int main (){//
+    //std::cout << "hello";
+   // return 0;
+//}//
+
+using namespace std;
+int main(){
+int x, y;
+cin >> x >> y ;
+cout<< "value of x:" << x << " value of y:" << y;
+}
+
