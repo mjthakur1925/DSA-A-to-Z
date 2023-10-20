9d3bfd8fe9a927afd1c85609f9c5d81ad6b79803
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2023-10-20 19:56:34 +0530

    Add files via upload

---
+++ C++/loops.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  
+   /* for (  int i = 1; i <=7; i++)
+    {
+        cout << "Hello World" << endl ;
+    }*/
+    int i =1;
+    while (i<=5)
+    {
+        cout<<"hello world"<<endl;
+        i =i +1;
+    }
+    return 0;
+    
+}
