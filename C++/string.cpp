b0bc4bbc5b98a8de9f9f86f9e96ab4cf709d27a2
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2023-10-21 20:18:58 +0530

    Add files via upload

---
+++ C++/string.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string m = "manisha";
+    int len = m.size(); // --> for findin length
+    m[len-1]='t'; //--> for changing the last index value from 'a' to 't'
+cout<< m[len -1];
+    return 0;
+}
