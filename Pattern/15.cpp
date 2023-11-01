05e825039194650f20775f08b5d2e49a8dee415a
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2023-11-01 11:47:56 +0530

    Add files via upload

---
+++ Pattern/15.cpp
@@ -0,0 +1,32 @@
+/*
+ABCDE
+ABCD
+ABC
+AB
+A
+*/
+ #include<bits/stdc++.h>
+       using namespace std;
+ void print(int n){
+   
+         for (int i = 0; i <n; i++)
+      {
+    
+    for ( char ch = 'A'; ch <= 'A' + (n-i-1); ch++)
+       {
+        cout<< ch<<" ";
+       
+       }
+ 
+      
+     
+     
+       cout<<endl;
+
+        }
+ }
+       int main (){
+        int n;
+        cin>>n;
+        print(n);
+       }
