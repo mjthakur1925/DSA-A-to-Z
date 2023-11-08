91a6eab2adf17a609fc6f07cdad67d0b489c8bad
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2023-11-08 17:46:47 +0530

    Add files via upload

---
+++ Recursion/Palindrome.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool palindrome( int i, string& s){
+    if (i>=s.length()/2)
+    return true;
+if(s[i]!=s[s.length()-i-1])
+    return false;
+
+    
+    
+   return palindrome(i+1,s);
+}
+int main(){
+ string s;
+ cin>>s;
+cout<<palindrome(0,s);
+	cout<<endl;
+	return 0;
+}
