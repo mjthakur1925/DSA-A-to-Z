10c41e961d05cad4ce115b256b69f0f6f80d381c
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2024-01-31 18:45:02 +0530

    Add files via upload

---
+++ Binary Tree/postorder.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void postOrderTraversal(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->left->right->left = new Node(8);
+    root->right->right->left = new Node(9);
+    root->right->right->right = new Node(10);
+
+    postOrderTraversal(root);
+
+    return 0;
+}
